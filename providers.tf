4a1866a41b4c17e2ab984e8d27442692023f9ec5
Sunil <sunil@digitalwallet.com>
2026-05-11 23:22:30 -0400

    fix deployment take 12

--- providers.tf
+++ providers.tf
@@ -1,8 +1,9 @@
 provider "azurerm" {
   features {}
-  use_cli         = false
-  use_oidc        = true
-  subscription_id = var.arm_subscription_id
+  use_cli                     = false
+  use_oidc                    = true
+  subscription_id             = var.arm_subscription_id
+  skip_provider_registration  = true
 }
 
 provider "random" {
